d2d682a945dacc62f06eed2035344e9fdce3bdab
Giovanni nocet <giovanni.noceti@gmail.com>
2022-08-14 13:51:31 -0400

    CHG: added project

---
+++ src/main/java/personalBackend/callaquen/CallaquenApplication.java
@@ -0,0 +1,13 @@
+package personalBackend.callaquen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallaquenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallaquenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personalBackend/callaquen/CallaquenApplicationTests.java
@@ -0,0 +1,13 @@
+package personalBackend.callaquen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallaquenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
